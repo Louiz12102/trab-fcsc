c5b4fa7660d683b9df7bd1fbdab2e4bb426aa5ce
zLucasLacerda <zlucaslacerda@gmail.com>
2026-06-22 09:43:24 -0300

    Refactor traffic flow calculations in TrafficController

--- src/controllers/TrafficController.js
+++ src/controllers/TrafficController.js
@@ -21,14 +21,12 @@ export const atualizarTransito = async (req, res, next) => {
         db.data.estadoTransito = {
             ...db.data.estadoTransito, 
             via1: {
-                filaMetros: (28 - via2.filaMetros) > 0 ? (38 - via2.filaMetros) : 0,
                 estado: via1.estado,
-                fluxo: calcularFluxo(28 - via1.filaMetros,28)
+                fluxo: db.data.estadoTransito.fluxo
             },
             via2: {
-                filaMetros:(38 - via2.filaMetros) > 0 ? (38 - via2.filaMetros) : 0,
                 estado: via2.estado,
-                fluxo: calcularFluxo(38 - via2.filaMetros,38)
+                fluxo: db.data.estadoTransito.fluxo
             },
             // Se o ESP32 enviar false (fim do ciclo), atualiza. Caso contrário, mantém true.
             pedestresAguardando: pedestresDoESP !== undefined ? pedestresDoESP : db.data.estadoTransito.pedestresAguardando,
